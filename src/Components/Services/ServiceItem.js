be6ad0767f58721b0e0a17ce6fb3979b29e7a956
abanoubmesiha <bony.nagy@gmail.com>
2020-03-10 15:43:40 +0200

    Service done

--- src/Components/Services/ServiceItem.js
+++ src/Components/Services/ServiceItem.js
@@ -3,12 +3,14 @@ import {Card} from 'react-bootstrap';
 
 export default function ServiceItem(props) {
     return (
-        <Card style={{ width: '18rem',boxShadow: '1px 1px 4px'}}>
+        <div className="col-lg-3 my-3">
+        <Card style={{ boxShadow: '1px 1px 4px'}}>
     <Card.Body>
       <Card.Title>{props.title}</Card.Title>
       <Card.Text style={{fontSize: '14px',color: '#c4c4c4'}}>{props.desc}</Card.Text>
       <img src={props.ImgUrl} style={{width:'100px'}} />
     </Card.Body>
   </Card>
+  </div>
     )
 }
