32f9b70fcb35345350b751b455e9fc318d566ecb
abanoubmesiha <bony.nagy@gmail.com>
2020-03-10 13:18:54 +0200

    rating done

--- src/Components/Rate/RateItem.js
+++ src/Components/Rate/RateItem.js
@@ -4,8 +4,8 @@ import './Rate.css';
 export default function RateItem(props) {
     return (
         <React.Fragment>
-            <div className="w-100 d-flex">
-                <div>
+            <div className="w-100">
+                
                 <label className="Flip">
         <input className="Flip" type="checkbox"  />
         <div className="Flipcard">
@@ -13,21 +13,21 @@ export default function RateItem(props) {
             <div className="Flipback ">
                 <h6 className="">كم هو تقييمك ؟</h6>
                 <fieldset className="rating">
-                <input type="radio" id="star5" name="rating" value="5" /><label className = "full" htmlFor="star5" title="Awesome - 5 stars"></label>
-                <input type="radio" id="star4half" name="rating" value="4 and a half" /><label className="half" htmlFor="star4half" title="Pretty good - 4.5 stars"></label>
-                <input type="radio" id="star4" name="rating" value="4" /><label className = "full" htmlFor="star4" title="Pretty good - 4 stars"></label>
-                <input type="radio" id="star3half" name="rating" value="3 and a half" /><label className="half" htmlFor="star3half" title="Meh - 3.5 stars"></label>
-                <input type="radio" id="star3" name="rating" value="3" /><label className = "full" htmlFor="star3" title="Meh - 3 stars"></label>
-                <input type="radio" id="star2half" name="rating" value="2 and a half" /><label className="half" htmlFor="star2half" title="Kinda bad - 2.5 stars"></label>
-                <input type="radio" id="star2" name="rating" value="2" /><label className = "full" htmlFor="star2" title="Kinda bad - 2 stars"></label>
-                <input type="radio" id="star1half" name="rating" value="1 and a half" /><label className="half" htmlFor="star1half" title="Meh - 1.5 stars"></label>
-                <input type="radio" id="star1" name="rating" value="1" /><label className = "full" htmlFor="star1" title="Sucks big time - 1 star"></label>
-                <input type="radio" id="starhalf" name="rating" value="half" /><label className="half" htmlFor="starhalf" title="Sucks big time - 0.5 stars"></label>
+                <input type="radio" id={`${props.Id}star5`} name={props.Id} value="5" /><label className = "full" htmlFor={`${props.Id}star5`} title="Awesome - 5 stars"></label>
+                <input type="radio" id={`${props.Id}star4half`} name={props.Id} value="4 and a half" /><label className="half" htmlFor={`${props.Id}star4half`} title="Pretty good - 4.5 stars"></label>
+                <input type="radio" id={`${props.Id}star4`} name={props.Id} value="4" /><label className = "full" htmlFor={`${props.Id}star4`} title="Pretty good - 4 stars"></label>
+                <input type="radio" id={`${props.Id}star3half`} name={props.Id} value="3 and a half" /><label className="half" htmlFor={`${props.Id}star3half`} title="Meh - 3.5 stars"></label>
+                <input type="radio" id={`${props.Id}star3`} name={props.Id} value="3" /><label className = "full" htmlFor={`${props.Id}star3`} title="Meh - 3 stars"></label>
+                <input type="radio" id={`${props.Id}star2half`} name={props.Id} value="2 and a half" /><label className="half" htmlFor={`${props.Id}star2half`} title="Kinda bad - 2.5 stars"></label>
+                <input type="radio" id={`${props.Id}star2`} name={props.Id} value="2" /><label className = "full" htmlFor={`${props.Id}star2`} title="Kinda bad - 2 stars"></label>
+                <input type="radio" id={`${props.Id}star1half`} name={props.Id} value="1 and a half" /><label className="half" htmlFor={`${props.Id}star1half`} title="Meh - 1.5 stars"></label>
+                <input type="radio" id={`${props.Id}star1`} name={props.Id} value="1" /><label className = "full" htmlFor={`${props.Id}star1`} title="Sucks big time - 1 star"></label>
+                <input type="radio" id={`${props.Id}starhalf`} name={props.Id} value="half" /><label className="half" htmlFor={`${props.Id}starhalf`} title="Sucks big time - 0.5 stars"></label>
             </fieldset></div>
         </div>
     </label>
-                    
-                </div>
+    <h6>{props.Id}</h6>  
+                
             </div>
         </React.Fragment>
     )
--- src/Components/Rate/index.js
+++ src/Components/Rate/index.js
@@ -9,7 +9,13 @@ export default function Rate() {
             <h3>قيمنا</h3>
             <div style={{margin:'0 20rem'}}>
             <h6>نعمل جاهدين في اتريك علي الحفاظ علي قيمنا الساميه التي نحرص علي تطبيقها في جميع اعمالنا ومعاملاتنا</h6>
-            <RateItem ImgUrl={require("../../assets/images/Group 2474@3x.png")}/>
+           <div className="d-flex">
+            <RateItem Id={'الابتكار'} ImgUrl={require("../../assets/images/Group 2478@3x.png")}/>
+            <RateItem Id={'الإلتزام'} ImgUrl={require("../../assets/images/Group 2477@3x.png")}/>
+            <RateItem Id={'الجوده'} ImgUrl={require("../../assets/images/Group 2476@3x.png")}/>
+            <RateItem Id={'المرونه'} ImgUrl={require("../../assets/images/Group 2475@3x.png")}/>
+            <RateItem Id={'الشغف'} ImgUrl={require("../../assets/images/Group 2474@3x.png")}/>
+            </div>
             </div>
         </div>
         </div>
