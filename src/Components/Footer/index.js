6f4d85288093edd4265dac724ac68eb0428bdb79
abanoubmesiha <bony.nagy@gmail.com>
2020-03-11 21:03:46 +0200

    finished except logo and background

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -5,7 +5,7 @@ export default function index() {
     return (
         <div className="w-100" style={{backgroundColor:'black',height:'20vh'}}>
         <div className="row">
-        <div className="col-12 col-12 d-flex justify-content-between">
+        <div className="col-12 d-flex justify-content-between">
         <ListGroup className="float-left" horizontal>
   <ListGroup.Item><img src={require('../../assets/images/Exclusion 3@3x.png')} width="40" /></ListGroup.Item>
   <ListGroup.Item><img src={require('../../assets/images/Exclusion 1@3x.png')} width="40" /></ListGroup.Item>
@@ -16,8 +16,14 @@ export default function index() {
   <ListGroup.Item>موقعنا</ListGroup.Item>
   <ListGroup.Item>خدماتنا</ListGroup.Item>
   <ListGroup.Item>من نحن</ListGroup.Item>
+  <img src={require('../../assets/images/Exclusion 4@3x.png')} width="100px" />
 </ListGroup>
-
+        </div>
+        </div>
+        <div className="row  d-flex justify-content-center">
+        <div className="col-10 text-center">
+<hr style={{border:'1px solid grey'}}/>
+<p>© 2020 Terkwaz All Rights Reserved</p>
         </div>
         </div>
         </div>
