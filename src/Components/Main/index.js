fc0b37a5c4951699aa354bcf5c8fd8eb0b1623e8
abanoubmesiha <bony.nagy@gmail.com>
2020-03-12 19:14:17 +0200

    responsive main

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 import {Row} from 'react-bootstrap'
 export default function Main() {
     return (
-            <div className="text-right text-white">
+            <div id="Main" className="desktop text-right text-white pr-3 pt-5">
              <h3 className="responsiveText">نقدم أفضل <span style={{color:'yellow'}}>حلول التسويق</span></h3>
              <h3 className="responsiveText">عبر المواقع والمنصات الإلكترونيه</h3>
              <h3 className="responsiveText">كافة ونضمن إعلام وحضور الجمهور</h3>
              </div>
+            
     )
 }
