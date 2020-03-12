1893ddb3a56924bb2110534e375f3192e300b31a
abanoubmesiha <bony.nagy@gmail.com>
2020-03-12 10:16:24 +0200

    imgs above done

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -8,22 +8,22 @@ export default function NavBar() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#" className="border-dark" style={{/*border:'0.1rem solid white',borderRadius: '1.5rem'*/}}>للتواصل معنا</Nav.Link>
+      <Nav.Link href="#" className="text-white" style={{border:'0.1rem solid white',borderRadius: '1.5rem'}}>للتواصل معنا</Nav.Link>
     </Nav>
     <Nav>
-           <Nav.Link href="#" className="">شركاؤنا</Nav.Link>
+           <Nav.Link href="#" className="text-white">شركاؤنا</Nav.Link>
          </Nav>
          <Nav>
-           <Nav.Link href="#Rate" className="">قيمنا</Nav.Link>
+           <Nav.Link href="#Rate" className="text-white">قيمنا</Nav.Link>
          </Nav>
          <Nav>
-           <Nav.Link href="#About" className=" smoothscroll">من نحن</Nav.Link>
+           <Nav.Link href="#About" className="text-white smoothscroll">من نحن</Nav.Link>
          </Nav>
          <Nav>
-           <Nav.Link href="#Main" className="">الرئيسيه</Nav.Link>
+           <Nav.Link href="#Main" className="text-white">الرئيسيه</Nav.Link>
          </Nav>
   </Navbar.Collapse>
-  <Navbar.Brand className="" href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand className="text-white" href="#home">React-Bootstrap</Navbar.Brand>
 </Navbar>
     </React.Fragment>
 
