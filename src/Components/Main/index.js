719bd130991342b6c048d9eb5d71494b73e9b1bd
abanoubmesiha <bony.nagy@gmail.com>
2020-03-13 18:41:17 +0200

    done 2

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Row} from 'react-bootstrap'
 export default function Main() {
     return (
-            <div id="Main" className="desktop text-right text-white pr-3 pt-5">
+            <div id="Main" className="desktop text-right text-white">
              <h3 className="responsiveText">نقدم أفضل <span id="ShoppingSol">حلول التسويق</span></h3>
              <h3 className="responsiveText">عبر المواقع والمنصات الإلكترونيه</h3>
              <h3 className="responsiveText">كافة ونضمن إعلام وحضور الجمهور</h3>
